4c22fced002e8b9302f4997047c077ed8cdb8df9
adriib38 <39964431+adriib38@users.noreply.github.com>
2022-02-23 00:55:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+import requests
+import time
+
+while True:
+    def precioBTC():
+        f = open('BTC.txt', 'r')
+        precio = f.read()
+
+        x = precio.split("/")
+        precioF = float(x[0])
+        print(f'BTC: {round(precioF, 2)}€')
+        f.close()
+
+    def precioETH():
+        f = open('ETH.txt', 'r')
+        precio = f.read()
+
+        x = precio.split("/")
+        precioF = float(x[0])
+        print(f'ETH: {round(precioF, 2)}€')
+        f.close()
+
+
+    headers = {
+            'X-CMC_PRO_API_KEY': '0914e058-72b9-46fd-b1e2-7c5b0659c564',
+            'Accepts': 'application/json'
+            }
+
+    params = {
+            'start': '1',
+            'limit': '6',
+            'convert': 'EUR'
+            }
+
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+    json = requests.get(url, params = params, headers = headers).json()
+
+    coins = json['data']
+
+    for coin in coins:
+        if coin['symbol'] == 'BTC':
+            file = open("./BTC.txt","w")
+            file.write(str(round(coin['quote']['EUR']['price'],5) ) + "\n" )
+            file.close();
+        if coin['symbol'] == 'ETH':
+            file = open("./ETH.txt","w")
+            file.write(str(round(coin['quote']['EUR']['price'],5) ) + "\n" )
+            file.close();
+
+    precioBTC()
+    precioETH()
+
+    time.sleep(60)
+
+
+
+
+
+
