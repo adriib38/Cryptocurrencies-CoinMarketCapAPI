8f2252d175f19df233012daf2dd8e604197dd7c3
Adrián <39964431+adriib38@users.noreply.github.com>
2022-10-27 16:37:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 headers = {
-        'X-CMC_PRO_API_KEY': 'YOUR_API_KEY',
+        'X-CMC_PRO_API_KEY': '0914e058-72b9-46fd-b1e2-7c5b0659c564',
         'Accepts': 'application/json'
         }
 
@@ -16,15 +16,24 @@ url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
 json = requests.get(url, params = params, headers = headers).json()
 
-coins = json['data']
+coins = json["data"]
+#print(coin)
 
 for coin in coins:
-    if coin['symbol'] == 'BTC':
-      print(coin['symbol'])
-      print(round(coin['quote']['EUR']['price'],5))
-    if coin['symbol'] == 'ETH':
-      print(coin['symbol'])
-      print(round(coin['quote']['EUR']['price'],5))
+      #print(coin['symbol'])
+      #print(round(coin['quote']['EUR']['price'],5))
+    
+      if coin['symbol'] == 'BTC':
+        #print(coin['symbol'])
+        #print(round(coin['quote']['EUR']['price'],5))
+
+        btc =	{
+
+        }
         
+        btc["live"] = round(coin['quote']['EUR']['price'])
+        btc["last_updated"] = coin['quote']['EUR']['last_updated']
 
+        print(btc)
 
+#DOCU: https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyListingsLatest
