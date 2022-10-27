46aac02f2568f6624105c1b45c0c4adf540b15ea
Adrián <39964431+adriib38@users.noreply.github.com>
2022-10-27 15:59:43 +0200

    remove files

--- main.py
+++ main.py
@@ -1,60 +1,30 @@
 import requests
 import time
 
-while True:
-    def precioBTC():
-        f = open('BTC.txt', 'r')
-        precio = f.read()
+headers = {
+        'X-CMC_PRO_API_KEY': 'YOUR_API_KEY',
+        'Accepts': 'application/json'
+        }
 
-        x = precio.split("/")
-        precioF = float(x[0])
-        print(f'BTC: {round(precioF, 2)}€')
-        f.close()
+params = {
+        'start': '1',
+        'limit': '6',
+        'convert': 'EUR'
+        }
 
-    def precioETH():
-        f = open('ETH.txt', 'r')
-        precio = f.read()
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
-        x = precio.split("/")
-        precioF = float(x[0])
-        print(f'ETH: {round(precioF, 2)}€')
-        f.close()
+json = requests.get(url, params = params, headers = headers).json()
 
+coins = json['data']
 
-    headers = {
-            'X-CMC_PRO_API_KEY': '[API-KEY]',
-            'Accepts': 'application/json'
-            }
-
-    params = {
-            'start': '1',
-            'limit': '6',
-            'convert': 'EUR'
-            }
-
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-
-    json = requests.get(url, params = params, headers = headers).json()
-
-    coins = json['data']
-
-    for coin in coins:
-        if coin['symbol'] == 'BTC':
-            file = open("./BTC.txt","w")
-            file.write(str(round(coin['quote']['EUR']['price'],5) ) + "\n" )
-            file.close();
-        if coin['symbol'] == 'ETH':
-            file = open("./ETH.txt","w")
-            file.write(str(round(coin['quote']['EUR']['price'],5) ) + "\n" )
-            file.close();
-
-    precioBTC()
-    precioETH()
-
-    time.sleep(60)
-
-
-
-
+for coin in coins:
+    if coin['symbol'] == 'BTC':
+      print(coin['symbol'])
+      print(round(coin['quote']['EUR']['price'],5))
+    if coin['symbol'] == 'ETH':
+      print(coin['symbol'])
+      print(round(coin['quote']['EUR']['price'],5))
+        
 
 
